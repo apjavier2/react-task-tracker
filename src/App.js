91f47d728051165b807cf01d3d07ea63e7897356
apjavier2 <apjavier27@gmail.com>
2023-02-21 09:02:37 +0800

    created first custom component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ExpenseItem() {
+    return <h2>Expense Item!</h2>
+}
+
+export default ExpenseItem;
