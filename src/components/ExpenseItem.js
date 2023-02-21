83e63f7a004f683dbe979433993932c62eec932b
apjavier2 <apjavier27@gmail.com>
2023-02-21 09:51:42 +0800

    added styling in expense item through css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  return (
+    <div className="expense-item">
+      <div>March 28, 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
