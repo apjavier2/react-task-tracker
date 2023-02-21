e54962081f69db36bed7c74515ee34423b0bafde
apjavier2 <apjavier27@gmail.com>
2023-02-21 14:00:30 +0800

    organized components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import ExpenseItem from "./ExpenseItem";
 import React from "react";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./Expenses.css";
 
 function Expenses(props) {
